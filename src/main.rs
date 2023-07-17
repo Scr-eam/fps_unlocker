2db98a4e94a7326020ed9a826a5d95930c57f743
Scr-eam <Scr3am@riseup.net>
2023-07-17 17:15:32 -0230

    now attempts to check registry for your current version

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{fs, io::{self, stdin, stdout, Write}, path::Path, {thread, time}};
 use directories::BaseDirs;
 use reqwest::{Error};
 use serde_json::Value;
+use winreg::{enums::HKEY_CURRENT_USER, RegKey};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -20,15 +21,15 @@ async fn main() -> Result<(), Error> {
     let version: String = if let Some(client_version_upload) = client_settings.get("clientVersionUpload") {
         client_version_upload.as_str().unwrap().to_string()
     } else {
-        println!("> roblox client version not found, lets try using another api instead\n");
+        println!("> roblox client version not found, lets try checking your registry instead\n");
+        
+        let hkey_current_user = RegKey::predef(HKEY_CURRENT_USER);
+        let roblox_key = hkey_current_user.open_subkey("SOFTWARE\\ROBLOX Corporation\\Environments\\roblox-player");
 
-        let new_req = reqwest::get("https://setup.rbxcdn.com/version")
-        .await?
-        .text()
-        .await?;
-
-        new_req
+        roblox_key.expect("unable to find roblox player registry").get_value("version").unwrap()
     };
+
+    println!("> found roblox version! {}\n", version);
 
     if let Some(proj_dirs) = BaseDirs::new() {
         let local_appdata = proj_dirs.cache_dir();
